935710becbddaf32c116db38f274a9ff4a4e8287
Caesar <caesar.keene@gmail.com>
2023-10-26 15:10:40 +0300

    addition of functions and 3 new variables

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ const generalButton = document.getElementById("general");
 const sportsButton = document.getElementById("sports");
 const searchButton = document.getElementById("searchButton");
 
+const newsQuery = document.getElementById("newsQuery");
+const newsType = document.getElementById("newsType");
+const newsdetails = document.getElementById("newsdetails");
+
 // Defining APIs 
+
+
+
+generalButton.addEventListener("click",function(){
+
+});
+
+sportsButtonButton.addEventListener("click",function(){
+
+});
+
+searchButtonButton.addEventListener("click",function(){
+
+});
