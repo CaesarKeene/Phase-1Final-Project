296a50c54ee30f47b2c2d9afb158650ac8b0f2c2
Caesar <caesar.keene@gmail.com>
2023-10-26 14:34:43 +0300

    changes made for navbar items and the variables on index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Declaring variables 
 
 const generalButton = document.getElementById("general");
-const businessButton = document.getElementById("business");
 const sportsButton = document.getElementById("sports");
-const techButton = document.getElementById("tech");
 const searchButton = document.getElementById("searchButton");
 
 // Defining APIs 
