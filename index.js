cc95ac9aa26e2a478f8a207e8250c726a73780a0
Caesar <caesar.keene@gmail.com>
2023-10-26 23:14:18 +0300

    added API links for the different news categories

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const newsdetails = document.getElementById("newsdetails");
 // Defining APIs 
 
 const API_KEY = "77024c19bab14cb98a48bd564d0aa020";
-const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=us&apiKey="
-const GENERAL_NEWS = ""
-const SPORTS_NEWS = ""
-const SEARCH_NEWS = ""
+const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" 
+const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey="
+const SPORTS_NEWS = "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey="
+const SEARCH_NEWS = "https://newsapi.org/v2/everything?q="
 
 
 generalButton.addEventListener("click",function(){
 
+    fetchGeneralNews();
 });
 
 sportsButton.addEventListener("click",function(){
